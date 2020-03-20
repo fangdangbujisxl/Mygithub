648ce0f61908744ff915c62cd8eca7e8fbdf0f90
shixiaolong <shiwanwujiewu@163.com>
2020-03-20 17:11:50 +0800

    第一次提交

---
+++ src/main/java/com/baidu/MyTest.java
@@ -0,0 +1,10 @@
+package com.baidu;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class MyTest {
+
+    public static void main(String[] args) {
+        System.out.println("菠菜贱卖");
+    }
+}
