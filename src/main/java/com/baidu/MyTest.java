ccdadf7180d3dff4f8ffd57250405dc9e7845fcb
shixiaolong <shiwanwujiewu@163.com>
2020-03-20 17:50:16 +0800

    第二次添加信息

--- src/main/java/com/baidu/MyTest.java
+++ src/main/java/com/baidu/MyTest.java
@@ -7,5 +7,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("菠菜贱卖");
         System.out.println("啦啦啦");
+        System.out.println("窝窝头");
     }
 }
